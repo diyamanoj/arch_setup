d5a8ad89bc7bbbe986faf38f52c39c01a85edc72
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2022-07-01 00:24:07 +0530

    athene_setup: build for device

--- athene_setup.sh
+++ athene_setup.sh
@@ -4,4 +4,11 @@ read choice
 if [ $choice == 'y' ]
 then
     rm -rf out/target/product/athene
+    . build/envsetup.sh
+    lunch lineage_athene-eng
+    mka bacon -j12
+else
+    . build/envsetup.sh
+    lunch lineage_athene-eng
+    mka bacon -j12
 fi
